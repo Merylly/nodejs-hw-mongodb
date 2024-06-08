ce901892a97d902a30faf419c29581cf87f5aa13
Merylly <142231027+Merylly@users.noreply.github.com>
2024-06-08 14:32:24 +0300

    v1

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,3 +1,4 @@
+import createHttpError from 'http-errors';
 import { Contact } from '../db/models/contacts.js';
 
 export const getContacts = async () => {
@@ -6,7 +7,35 @@ export const getContacts = async () => {
 };
 
 export const getContactsById = async (contactId) => {
-
   const contact = Contact.findById(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Student not found');
+  }
+
   return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await Contact.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await Contact.findByIdAndUpdate(contactId, payload, {
+    new: true,
+    includeResultMetadata: true,
+    ...options,
+  });
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  await Contact.findByIdAndDelete(contactId);
 };
